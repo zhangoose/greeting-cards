944289cb15a875be5e49d7f254863539729a41e8
Alex Zhang <az177@eden.rutgers.edu>
2013-06-14 00:09:41 -0400

    Updated & done

--- bonvoyage/script.js
+++ bonvoyage/script.js
@@ -1,21 +1,60 @@
 var c = document.getElementById('screen');
 var ctx = c.getContext("2d");
-var WIDTH = c.width;  
+var WIDTH = c.width;	
 var HEIGHT = c.height;	
 var bg = new Image();
 var star = new Image();
 var plane = new Image();
 var x, y;
+var ytheo; // theoretical y
+var miss = 0;
+var hit = 0;
+var done = 0; // will be 1 when finished
+var scream = new Audio("scream.mp3");
+var welcome = new Audio("welcome.mp3");
+var clown = new Image();
+var fin = new Image();
+clown.src = "clown.png";
+fin.src = "final.png";
+
 
 
 function mouse(mevent){
-	if(mevent.pageX < WIDTH && mevent.pageX > 0 &&
-		mevent.pageY < HEIGHT && mevent.pageY > 0){
-		x = mevent.pageX;
-		y = mevent.pageY;
-		console.log("x: " + x + " y: " + y);
-		airplane();
-	}
+	if(done == 0){ // THIS IS SO DUMB
+		if(mevent.pageX < WIDTH && mevent.pageX > 0 &&
+			mevent.pageY < HEIGHT && mevent.pageY > 0){
+			x = mevent.pageX;
+			y = mevent.pageY;
+			//console.log("x: " + x + " y: " + y);
+			airplane();
+			var ytheo = 131 * x / 523 + 50780/523;
+			
+			if(869 + 5 >= x && x >= 869 - 5 && ytheo + 1 >= y && ytheo - 1 <= y){
+				done = 1;
+				clearInterval(inter);
+				ctx.drawImage(fin,0,0);
+				welcome.play();
+				
+			}/*end of if*/
+			if(ytheo + 1 >= y && ytheo - 1 <= y){
+				
+				//console.log("rawr");
+				//sound.play();
+				hit++;
+				document.getElementById("hit").innerHTML = "HIT: " + hit;
+				
+			}
+			else{
+				
+				ctx.drawImage(clown,0,0);
+				scream.play();
+				miss++;
+				document.getElementById("miss").innerHTML = "MISS: " + miss;
+				
+			}
+		}
+	}/*end of everything*/
+	
 }/*end of mouse function*/
 
 $(document).mousemove(mouse);
@@ -37,6 +76,7 @@ function mdraw(){
 	
 	star.src = "star.gif";
 	ctx.drawImage(star, 290,150);
+	ctx.drawImage(star,849,287);
 	airplane();
 	
 
