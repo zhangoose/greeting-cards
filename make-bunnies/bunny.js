50af1faf3552583ffa2dbfcb73ace7878efd8962
zhangoose <az177@eden.rutgers.edu>
2013-08-18 01:17:43 -0400

    extending bunny, made bunny object

---
+++ make-bunnies/bunny.js
@@ -0,0 +1,10 @@
+// for the bunny/poop objects
+
+function Bunny(xCoord, yCoord, type){
+	
+	this.xCoord = xCoord; //center x
+	this.yCoord = yCoord; // center y
+	this.type = type; //type = color of bunny or poop, taken from the arrayBunniez from script.js
+
+}//end of Bunny object initializer
+
